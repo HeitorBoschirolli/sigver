641cf598c140ac35c5d3f5d48cac3118afedfe2d
HeitorBoschirolli <heitor.boschirolli@gmail.com>
2019-02-17 16:28:47 -0300

    adds utsig in dataset options

--- sigver/datasets/__init__.py
+++ sigver/datasets/__init__.py
@@ -2,9 +2,11 @@ from .gpds import GPDSDataset
 from .mcyt import MCYTDataset
 from .cedar import CedarDataset
 from .brazilian import BrazilianDataset, BrazilianDatasetWithoutSimpleForgeries
+from .utsig import UTSIGDataset
 
 available_datasets = {'gpds': GPDSDataset,
                       'mcyt': MCYTDataset,
                       'cedar': CedarDataset,
                       'brazilian': BrazilianDataset,
-                      'brazilian-nosimple': BrazilianDatasetWithoutSimpleForgeries}
+                      'brazilian-nosimple': BrazilianDatasetWithoutSimpleForgeries,
+                      'utsig': UTSIGDataset}
